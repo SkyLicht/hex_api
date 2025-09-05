cd9fa271a6bb2ae17b94a1e2f8c4b8aa75d85ec6
skylicht <skylichtdev@gmail.com>
2025-09-05 17:22:35 -0600

    bro 5

--- db_tool.py
+++ db_tool.py
@@ -1,4 +1,6 @@
-from scripts.create_pop_work_plan import populate_work_plan
+from scripts.create_pop_work_plan import populate_work_plan, create_pop_work_plan_tables
 
 if __name__ == '__main__':
+    # create_pop_work_plan_tables()
+
     populate_work_plan()
